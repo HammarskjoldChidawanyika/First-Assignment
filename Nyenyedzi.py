f9fd1201aee1d74b9660f7a91e17f224f19741c0
Hammarskjold Chidawanyika <hammarskjoldc@yahoo.com>
2019-04-21 11:09:27 +0100

    Commit Nyenyedzi Assignment

---
+++ Nyenyedzi.py
@@ -0,0 +1,130 @@
+Song = "Nyenyedzi"
+
+Artist = "Tanga WeKwaSando"
+
+Year = 2001
+
+Genre = "Jazz"
+
+DurationInSeconds = 420
+
+MessageOfSong = "This song is about a boy wooing a girl"
+
+# He first proposes if the girl wants to know why he loves her#
+
+# she should listen. He goes on to say he does not mind where she comes from, what her totem is or her race
+
+# the rest of the song is sel-explanatory
+
+ConditionsOfOffer = "If you want to know, why I love you, listen"
+
+# The lyric above is sung twice before going on to the next one.
+
+ConditionsOfLove1 = "It does not matter, where you come from"
+
+ConditionsOfLove2 = "It does not matter, your totem"
+
+ConditionsOfLove3 = "It does not matter, where you come from"
+
+ConditionsOfLove4 = "It does not matter, your race"
+
+AppreciationOfGirl1 = "How besotted am I, about your little love, from the fountain, of your heart"
+
+# The lyric above is sung twice before going on to the next one.
+
+AppearanceOfGirl1 = "Your eyes are sooo beautiful,stars of a Saturday morning"
+
+# The lyric above is sung twice before going on to the next one.
+
+# Instrumental part baseline bass guitar, saxophone, lead guitar
+
+AuraAtmosphere1 = "Stars of a Saturday morning"
+
+# Instrumental part baseline bass guitar, saxophone, lead guitar
+
+# The lyric above is sung twice before going on to the next one.
+
+AppearanceOfGirl2 = "Your eyes are sooo beautiful"
+
+AuraAtmosphere2 = "Ohhhh, stars of a Saturday morning"
+
+AppearanceOfGirl3 = "Your eyes are sooo beautiful"
+
+AuraAtmosphere3 = "Stars of a Saturday morning"
+
+AppearanceOfGirl4 = "Your face your face is so beautiful"
+
+AuraAtmosphere4 = "Stars of a Saturday morning"
+
+AppearanceOfGirl5 = "Your eyes are sooo beautiful"
+
+AuraAtmosphere5 = "Mmmm stars of a Saturday morning"
+
+AuraAtmosphere6 = "Mmmm mmm mm stars of a Saturday morning"
+
+AppearanceOfGirl6 ="Your eyes your eyes are sooo beautiful"
+
+AuraAtmosphere7 = "Mmmm stars of a Saturday morning"
+
+AppearanceOfGirl8 = "Your eyes are sooo beautiful"
+
+print(Song)
+
+print(Artist)
+
+print(Year)
+
+print(Genre)
+
+print(DurationInSeconds)
+
+print(MessageOfSong)
+
+print(ConditionsOfOffer)
+
+print(ConditionsOfOffer)
+
+print(ConditionsOfLove1)
+
+print(ConditionsOfLove2)
+
+print(ConditionsOfLove3)
+
+print(ConditionsOfLove4)
+
+print(AppreciationOfGirl1)
+
+print(AppreciationOfGirl1)
+
+print(AppearanceOfGirl1)
+
+print(AppearanceOfGirl1)
+
+print(AuraAtmosphere1)
+
+print(AuraAtmosphere1)
+
+print(AppearanceOfGirl2)
+
+print(AuraAtmosphere2)
+
+print (AppearanceOfGirl3)
+
+print(AuraAtmosphere3)
+
+print(AppearanceOfGirl4)
+
+print(AuraAtmosphere4)
+
+print(AppearanceOfGirl5)
+
+print(AuraAtmosphere5)
+
+print(AuraAtmosphere6)
+
+print(AppearanceOfGirl6)
+
+print(AuraAtmosphere7)
+
+print(AppearanceOfGirl8)
+
